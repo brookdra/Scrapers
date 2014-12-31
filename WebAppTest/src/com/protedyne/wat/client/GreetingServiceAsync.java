4cd4a6c052fa530757546a1ad1dfba6a83759368
daveb <daveb@protedyne.com>
2014-12-31 14:07:08 -0500

    Testing Git Staging View

--- WebAppTest/src/com/protedyne/wat/client/GreetingServiceAsync.java
+++ WebAppTest/src/com/protedyne/wat/client/GreetingServiceAsync.java
@@ -3,7 +3,7 @@ package com.protedyne.wat.client;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
- * The async counterpart of <code>GreetingService</code>.
+ * The async counterpart of <code>GreetingService</code>. drb
  */
 public interface GreetingServiceAsync {
 	void greetServer(String input, AsyncCallback<String> callback)
