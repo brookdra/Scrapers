afb903f6713bb618fdd779b333aebfcc56db6bc2
daveb <daveb@protedyne.com>
2014-12-31 15:18:05 -0500

    cause conflict

--- WebAppTest/src/com/protedyne/wat/client/GreetingService.java
+++ WebAppTest/src/com/protedyne/wat/client/GreetingService.java
@@ -4,7 +4,7 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
- * The client-side stub for the RPC service. drb
+ * The client-side stub for the RPC service. drb this should cause conflict
  */
 @RemoteServiceRelativePath("greet")
 public interface GreetingService extends RemoteService {
